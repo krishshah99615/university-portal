0ab627a46c83b5e1484abf8c781057f593679ee0
krishshah99615 <krishshah99615@gmail.com>
2019-10-27 00:37:16 +0530

    Second commit

--- student/urls.py
+++ student/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 app='student'
 urlpatterns = [
-    
-    path('student_login',view=views.student_login,name='student_login')
+
+    path('student_login',view=views.student_login,name='student_login'),
+    path('student_register',view=views.student_register,name='student_register'),
 ]
--- student/views.py
+++ student/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def student_login(request):
     return render(request,'student_login.html')
+def student_register(request):
+    return render(request,'student_register.html')
