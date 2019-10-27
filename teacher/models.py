cfae06848fd6342f5429c0f50b6194cf13182a62
krishshah99615 <krishshah99615@gmail.com>
2019-10-27 20:49:35 +0530

    Teacher Database

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from student.models import Staff
+department_choice=['Maths','Comp','Music']
+class Teacher(models.Model):
+    staff=models.ForeignKey(Staff,on_delete=True)
+    teacher_name=models.CharField(max_length=200)
+    department=models.IntegerField(choices=((i,x) for i,x in enumerate(department_choice)),default=0)
+    
