1a3aa359040a61d8a9b39e8bb8b8b84095e36f53
krishshah99615 <krishshah99615@gmail.com>
2019-10-28 16:26:42 +0530

    made student reg page final

--- student/forms.py
+++ student/forms.py
@@ -1,5 +1,11 @@
 from django import forms
-class StudentForm(forms.Form):
-    stud_name=forms.CharField()
-    section=forms.IntegerField()
-    year=forms.IntegerField()
+from .models import Staff,Student
+class StaffForm(forms.ModelForm):
+    class Meta:
+        model = Staff
+        fields = ('id', 'password')
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ('stud_name', 'section','year')
--- student/views.py
+++ student/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from .forms import StudentForm
+from .forms import StudentForm,StaffForm
+from .models import Staff,Student
 # Create your views here.
 def student_login(request):
     return render(request,'student_login.html')
 def student_register(request):
+    student_form=StudentForm()
+    staff_form=StaffForm()
+    if request.method=='POST':
+        student_form=StudentForm(request.POST)
+        staff_form=StaffForm(request.POST)
+        if staff_form.is_valid() and student_form.is_valid():
+            user = staff_form.save(commit=False)
+            user.save()
+
+            user.student_profile.stud_name = student_form.cleaned_data.get('stud_name')
+            user.student_profile.section =  student_form.cleaned_data.get('section')
+            user.student_profile.year =  student_form.cleaned_data.get('year')
+
+            user.student_profile.save()
+
+
+
     context={
-             'form':StudentForm()
+             'form':staff_form,'form2':student_form
     }
     return render(request,'student_register.html',context)
