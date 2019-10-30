16c116c24d645ef5c5d148642ba8ad304b24b068
krishshah99615 <krishshah99615@gmail.com>
2019-10-30 14:59:26 +0530

    reg and login done.finally

--- student/admin.py
+++ student/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import Staff,Student
-admin.site.register(Staff)
+
 admin.site.register(Student)
 # Register your models here.
--- student/models.py
+++ student/models.py
@@ -5,52 +5,93 @@ year_choice=['First Year','Second Year','Third Year','Fourth Year']
 
 
 class MyStaffManager(BaseUserManager):
-    def create_user(self,id,password):
+    def create_user(self, id, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
         if not id:
-            raise ValueError("id is required")
-        if not password:
-            raise ValueError("password required")
+            raise ValueError('Users must have an id ')
+
         user = self.model(
-                          id=id,
+            id=id,
 
         )
+
         user.set_password(password)
         user.save(using=self._db)
         return user
-    def create_superuser(self,id,password):
-        user = self.create_user(
-                          id=id,
-                          password=password,
-        )
-        user.is_admin=True
-        user.is_staff=True
-        user.is_superuser=True
 
+    def create_superuser(self, email,  password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(id,
+            password=password,
+
+        )
+        user.is_admin = True
         user.save(using=self._db)
         return user
-
-
-
 
 
 class Staff(AbstractBaseUser):
     id=models.IntegerField(unique=True,primary_key=True)
     password=models.CharField(max_length=100)
-    is_admin =models.BooleanField(default=True)
-    is_staff=models.BooleanField(default=True)
-    is_superuser=models.BooleanField(default=True)
+    is_admin =models.BooleanField(default=False)
+    is_active=models.BooleanField(default=True)
     is_teacher=models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'id'
-    REQUIRED_FIELDS=[]
-    objects=MyStaffManager()
+    objects = MyStaffManager()
 
-    def __str__(self):
-        return str(self.id)
-    def has_perm(self,perm,obj=None):
-        return self.is_admin
-    def has_module_perms(self,app_label):
+    USERNAME_FIELD = 'id'
+    REQUIRED_FIELDS = []
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.id
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.id
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.id
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
         return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+    @property
+    def is_teacher(self):
+        "Is the user teacher?"
+        # Simplest possible answer: All admins are staff
+        return self.is_teacher
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Student(models.Model):
     staff=models.OneToOneField(Staff,on_delete=models.CASCADE,null=True,related_name='student_profile')
     stud_name=models.CharField(max_length=200)
